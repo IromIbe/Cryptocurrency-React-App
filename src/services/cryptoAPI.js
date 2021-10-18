f2995ca6f33eb1665610eedd12436dfe90dabb3f
heiscostly <ayangcostly@gmail.com>
2021-10-18 10:50:08 +0100

    My First Commit

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "50dc004297msh623c2d0380ee56bp104e2ejsn45200053fccc",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+
+const createRequest = (url) => ({
+  url,
+  headers: cryptoApiHeaders,
+});
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
+
+// var options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/exchanges",
+//   headers: {
+//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+//     "x-rapidapi-key": "50dc004297msh623c2d0380ee56bp104e2ejsn45200053fccc",
+//   },
+// };
